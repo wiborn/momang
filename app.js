940a91e06e66514f4c025945bde2ca4877b5e6a0
Joakim Wiborn <joakim.wiborn@gmail.com>
2023-02-24 07:59:19 +0100

    add aff link

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const helmet = require('helmet');
-const { ErrorResponseObject } = require('./common/http');
-const routes = require('./routes');
+const express = require("express");
+const helmet = require("helmet");
+const { ErrorResponseObject } = require("./common/http");
+const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(helmet());
-app.use('/', routes);
-
 // default catch all handler
-app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
+app.all("*", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
 
 module.exports = app;
